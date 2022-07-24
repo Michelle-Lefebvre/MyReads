20dc7a4576e4758b7f1e2c935d3216340dcde145
Michelle Lefebvre <mmldesigns007@gmail.com>
2022-07-23 23:46:53 -0400

    built book.js

--- starter/src/components/Book.js
+++ starter/src/components/Book.js
@@ -1,11 +1,51 @@
+import { useState, useEffect } from 'react';
+import * as BooksAPI from '../BooksAPI';
+
 const Book = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const getBook = async () => {
+      const res = await BooksAPI.getAll();
+      setBooks(res);
+    };
+
+    getBook();
+  }, []);
   return (
     <div>
-      <h1>Book</h1>
-      <h2>Title</h2>
-      <h3>Authors:</h3>
-      <p>name</p>
-      <p>name</p>
+      <ol className='books-grid' style={{ 'list-style-type': 'none' }}>
+        {books.map((book) => (
+          <li key={book.id}>
+            <div className='book'>
+              <div className='book-top'>
+                <div
+                  className='book-cover'
+                  style={{
+                    width: 128,
+                    height: 193,
+                    backgroundImage: `url(${book.imageLinks.thumbnail})`,
+                  }}
+                ></div>
+                <div className='book-shelf-changer'>
+                  <select>
+                    <option value='none' disabled>
+                      Move to...
+                    </option>
+                    <option value='currentlyReading'>Currently Reading</option>
+                    <option value='wantToRead'>Want to Read</option>
+                    <option value='read'>Read</option>
+                    <option value='none'>None</option>
+                  </select>
+                </div>
+              </div>
+              <div className='book-title'>{book.title}</div>
+              <div className='book-authors'>{book.authors.join(', ')}</div>
+              <small>{book.shelf}</small>
+            </div>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 };
--- starter/src/components/BookList.js
+++ starter/src/components/BookList.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Book from './Book';
 
 const BookList = ({ books }) => {
   const [query, setQuery] = useState('');
@@ -12,6 +13,7 @@ const BookList = ({ books }) => {
     updateQuery('');
   };
 
+  // eslint-disable-next-line no-unused-vars
   const showingBooks =
     query === ''
       ? books
@@ -21,39 +23,7 @@ const BookList = ({ books }) => {
     <div className='list-books-content'>
       All Books List
       <div className='bookshelf-books'>
-        <ol className='books-grid' style={{ 'list-style-type': 'none' }}>
-          {showingBooks.map((book) => (
-            <li>
-              <div className='book'>
-                <div className='book-top'>
-                  <div
-                    className='book-cover'
-                    style={{
-                      width: 128,
-                      height: 193,
-                      backgroundImage: `url(${book.imageLinks.thumbnail})`,
-                    }}
-                  ></div>
-                  <div className='book-shelf-changer'>
-                    <select>
-                      <option value='none' disabled>
-                        Move to...
-                      </option>
-                      <option value='currentlyReading'>
-                        Currently Reading
-                      </option>
-                      <option value='wantToRead'>Want to Read</option>
-                      <option value='read'>Read</option>
-                      <option value='none'>None</option>
-                    </select>
-                  </div>
-                </div>
-                <div className='book-title'>{book.title}</div>
-                <div className='book-authors'>{book.authors.join(', ')}</div>
-              </div>
-            </li>
-          ))}
-        </ol>
+        <Book shelf={books.shelf} />
       </div>
     </div>
   );
